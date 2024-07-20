8c6a257741cce7b19cd39887a904ac273bc5a18a
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2024-07-20 18:11:46 -0300

    idk

--- lua/gab/remap.lua
+++ lua/gab/remap.lua
@@ -1,2 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Remap <leader>y to "+y in normal and visual modes
+vim.api.nvim_set_keymap('n', '<leader>y', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true, silent = true })
